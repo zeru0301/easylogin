0c103abee77828c530a3e6956c1d89901da7ec9b
zeru0301 <airisk1124@icloud.com>
2024-11-29 15:10:27 +0900

    easyloginの制作物です確認をお願いします。

---
+++ src/main/java/jp/co/internous/easylogin/model/domain/MstUser.java
@@ -0,0 +1,45 @@
+package jp.co.internous.easylogin.model.domain;
+
+public class MstUser {
+	
+	private int id;
+	
+	private String userName;
+	
+	private String password;
+	
+	private String fullName;
+	
+	public int getid() {
+		return id;
+	}
+	
+	public void setid(int id) {
+		this.id = id;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassworld(String password) {
+		this.password = password;
+	}
+	
+	public String getFullName() {
+		return fullName;
+	}
+	
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+}
